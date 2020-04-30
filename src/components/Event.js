cd6d710e7e3d511e8a9a2356dce255c9b4059d21
xu_fu <xu_fu@hunliji.com>
2020-04-30 15:12:08 +0800

    fix

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,11 +8,11 @@ export default class extends Component {
     console.log('event target', event.target)
     console.log('event currentTarget', event.currentTarget)
     console.log('event constructor', event.__proto__.constructor)
-    
+
     console.log('nativeEvent target', event.nativeEvent.target)  // 所有的事件都被挂载在document上
-    console.log('nativeEvent target', event.nativeEvent.currentTarget)
+    console.log('nativeEvent currentTarget', event.nativeEvent.currentTarget)
     console.log('nativeEvent constructor', event.nativeEvent.__proto__.constructor)
-    
+
 
     // event其实是React封装出来的，他的构造函数是SyntheticEvent(可以通过event.__proto__.constructor查看)，模拟出了DOM事件的所有能力
     // event.nativeEvent 是原生事件对象(构造函数是MouseEvent)
@@ -29,7 +29,7 @@ export default class extends Component {
     return (
       <div>
         event page
-        <div className='aTagWrapper' onClick={this.onWrapperClick}>
+        <div className='aTagWrapper' onClick={this.onWrapperClick} style={{ border: '1px solid red' }}>
           <a href='aaa' onClick={this.onClick}>跳转</a>
         </div>
       </div>
